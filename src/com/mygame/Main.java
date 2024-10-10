b0deba40049a31c659df4490f06234afd6c0e027
Abhishek Ambi <109055711+CSEStudentAbhi@users.noreply.github.com>
2024-10-10 11:21:24 +0530

    commit

---
+++ src/com/mygame/Main.java
@@ -0,0 +1,10 @@
+
+package com.mygame;
+import com.mygame.TicTacToe;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Program is Starting...");
+        
+        TicTacToe game  = new TicTacToe();
+    }
+}
